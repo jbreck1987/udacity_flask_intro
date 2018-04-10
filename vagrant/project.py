e87dee59399ad5b12a3f03dc8cb602d8f3a13829
Josh Breckenridge <bloo1987@gmail.com>
2018-04-10 11:26:01 -0400

    Updated app to display restaurant menu items

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from database_setup import create_db_session, Restaurant, MenuItem
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def print_hello():
-    return 'Hello World!'
+@app.route('/restaurants/')
+def print_restaurants():
+    session = create_db_session()
+
+    restaurant = session.query(Restaurant).first()
+    menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant.Id)
+
+    session.close()
+
+    output = ''
+    for item in menu_items:
+        output += '{}'.format(item.name)
+        output += '</br>'
+        output += '{}'.format(item.description)
+        output += '</br>'
+        output += '{}'.format(item.price)
+        output += '</br>'
+        output += '</br>'
+
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
